aa9daba75ea90e5477a4dd1167f742bbadf9799f
defaulterrr <thedefaulterrr@gmail.com>
2021-02-10 00:11:29 +0300

    Initial Commit

---
+++ test.first/test_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_firstApp.swift
+//  test.first
+//
+//  Created by Svyat Petrov on 10.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct test_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
